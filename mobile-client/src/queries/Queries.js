801b3902f85b6ea31a4b687f6a5debfa683c3630
DanielVasquezV <dvventura80@gmail.com>
2024-10-11 16:02:25 -0600

    fixing errors

--- mobile-client/src/queries/Queries.js
+++ mobile-client/src/queries/Queries.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //! host Direction of the server.
 // const host = 'https://medikids-server.uc.r.appspot.com';
-const host = 'http://192.168.1.6:5005'
+// const host = 'http://192.168.1.6:5005'
+const host = 'https://medikids-server.vercel.app';
 
 //\\ const of headers
 const headers_public = { headers: {'Content-Type': 'application/json'} }
