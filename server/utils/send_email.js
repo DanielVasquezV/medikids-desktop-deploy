fac89c1a554894ad5f13b4c83ef04f4d815d8794
DanielVasquezV <dvventura80@gmail.com>
2024-10-11 16:48:28 -0600

    fixing send_email

--- server/utils/send_email.js
+++ server/utils/send_email.js
@@ -1,34 +1,48 @@
 import nodemailer from 'nodemailer';
 import { config_env } from './dotenv_conf.js';
 
-const SendEmail = (options) => {
-  // SET THE CONFIG TO THE TRANSPORT THE EMAIL
+const SendEmail = async (options) => {
+  // Crear el transportador
   const transporter = nodemailer.createTransport({
-      host: config_env.EMAIL_HOST,
-      port: config_env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: config_env.EMAIL_USERNAME,
-        pass: config_env.EMAIL_PASSWORD
+    host: config_env.EMAIL_HOST,
+    port: config_env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: config_env.EMAIL_USERNAME,
+      pass: config_env.EMAIL_PASSWORD,
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log("Error al verificar el transportador:", error);
+        reject(error);
+      } else {
+        console.log("Servidor de correo listo para enviar mensajes");
+        resolve(success);
       }
     });
-  
-    // SET THE ELEMENTS OF THE EMAIL
-    const mailOptions = {
-      from: config_env.EMAIL_FROM,
-      to: options.to,
-      subject: options.subject,
-      html: options.text
-    }
-  
-    // FUNCTION TO SEND THE EMAIL AND GET BACK THE INFO
-    transporter.sendMail(mailOptions, function (err, info) {
+  });
+
+  const mailOptions = {
+    from: config_env.EMAIL_FROM,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
-        console.log(err)
+        console.error("Error al enviar el correo:", err);
+        reject(err);
       } else {
-        console.log(info)
+        console.log("Correo enviado con éxito:", info);
+        resolve(info);
       }
-    })
-} 
+    });
+  });
+};
 
 export { SendEmail };
